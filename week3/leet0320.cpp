b2ea0aa8caff8743f83ce5397b5d7f5b6f56c084
junyoung <abelois21@gmail.com>
2021-03-20 11:28:06 +0900

    leet

---
+++ week3/leet0320.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+bool canVisitAllRooms(vector<vector<int>>& rooms) {
+    int room, key;
+    queue<int> waits;
+    unordered_map<int, bool> um;
+
+    waits.push(0);
+    um[0] = 1;
+    while(waits.empty()) {
+	room = waits.front();
+	waits.pop();
+
+	for(int i = 0 ; i < (int)rooms[room].size(); i++) {
+	    key = rooms[room][i];
+	    if(um[key]) {
+		continue;
+	    }
+	    else {
+		waits.push(key);
+		um[key] = 1;
+	    }
+	}
+    }
+
+    if(rooms.size() == um.size()) {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+}
+
+int main(int argc, char** argv) {
+    vector<vector<int>> rooms;
+    vector<int> v;
+    v.push_back(1);
+    rooms.push_back(v);
+    v.pop_back();
+    v.push_back(2);
+    rooms.push_back(v);
+    v.pop_back();
+    v.push_back(3);
+    rooms.push_back(v);
+    v.pop_back();
+    rooms.push_back(v);
+    cout << canVisitAllRooms(rooms);
+    return 0;
+}
